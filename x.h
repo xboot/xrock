228b6b16fede3f67ccac7f2216fe9386cdf13d4c
JianjunJiang <8192542@qq.com>
2024-09-13 13:11:15 +0800

    [x.h]add define _FILE_OFFSET_BITS = 64

--- x.h
+++ x.h
@@ -5,23 +5,7 @@
 extern "C" {
 #endif
 
-#if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
-# define __WINDOWS__
-#endif
-#if defined(__linux__) || defined(__CYGWIN__)
-# include <endian.h>
-#elif defined(__MACH__)
-# include <sys/types.h>
-#elif defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
-# include <sys/endian.h>
-#elif defined(__WINDOWS__)
-# include <winsock2.h>
-# include <sys/param.h>
-# include <sys/time.h>
-#else
-# error "platform not supported!"
-#endif
-
+#define _FILE_OFFSET_BITS 64
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
